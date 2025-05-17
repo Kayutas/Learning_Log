1efd917ee22af810e685c8c2be95f8d8477d1981
Kayutas <aleksatasovac94@gmail.com>
2025-05-17 02:21:18 +0200

    added topics

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -7,6 +7,24 @@ class Topic(models.Model):
     """A Topic the user is learning about."""
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
-    def _str_(self):
+    def __str__(self):
         """Return a string representation of the model."""
         return self.text
+    
+    
+    
+
+
+class Entry(models.Model):
+    """Someting specific learned about a topic."""
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        verbose_name_plural = 'entries'
+        
+    def __str__(self):
+        """Return a simple string representation of the entry."""
+        return f"{self.text[:50]}..."
+    # The [:50] slice limits the string to the first 50 characters.
